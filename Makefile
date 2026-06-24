27b01f1e6950a8eb0c448bd659c9de43ec41f9ea
Giuliano Pagliei <gipaglie@c2r4s1.42roma.it>
2026-06-24 16:51:16 +0200

    insertion sort

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 CC = cc
-CFLAGS = -Wall -Wextra -Werror
+CFLAGS = -Wall -Wextra -Werror #-fsanitize=address,leak -g
 NAME = push_swap
 
-SRCS = push_swap.c utils.c parser.c moves.c lst_utils.c check_lst.c insertion_sort2.c
+SRCS = push_swap.c utils.c parser.c moves.c lst_utils.c check_lst.c insertion_sort_2.c
 OBJS := $(SRCS:.c=.o)
 
 all: $(NAME)
--- insertion_sort_2.c
+++ insertion_sort_2.c
@@ -1,14 +1,55 @@
 #include "push_swap.h"
 
 
-void	put_all_in_b_except_min(a, b)
+void	push_all_in_b_except_min(t_list **a, t_list **b)
 {
+	int min;
 
+	min = lesser_number(*a);
+	while((*a)->next)
+	{
+		if ((*a)->nb == min)
+			rotate(a, 'a');
+		push(b, a, 'b');
+	}
+	//ft_lstprint(*a, *b);
 }
 
+void	push_back_in_a(t_list **a, t_list **b)
+{
+	while(*b)
+	{
+		if ((*b)->next)
+		{
+			if(target_distance(greatest_number(*b), *b) >= (ft_lstsize(*b) / 2))
+			{
+				while((*b)->nb != greatest_number(*b))
+					rotate(b, 'b');
+			}
+			else			
+			{
+				while((*b)->nb != greatest_number(*b))
+					reverse_rotate(b, 'b');
+			}
+		}
+		push(a, b, 'a');
+	}
+}
 
 void	insertion_sort(t_list **a, t_list **b)
 {
-	put_all_in_b_except_min(a, b);
-	ft_lstprint(*a, *b);
+	//int lst_size;
+
+	//lst_size = ft_lstsize(*a);
+	push_all_in_b_except_min(a, b);
+	push_back_in_a(a, b);
+	//ft_lstprint(*a, *b);
+	reverse_rotate(a, 'a');
+	//ft_lstprint(*a, *b);
+	/*
+	if (ordered_but_need_rotation(*a))
+		{
+			rotate_to_order(a);
+		}
+	*/
 }
