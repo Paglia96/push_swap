76a5d6b06a875a28c6b3165cb4d54bb35989b640
Giuliano Pagliei <gipaglie@c2r4s1.42roma.it>
2026-07-07 14:59:40 +0200

    updated double_moves

--- double_moves.c
+++ double_moves.c
@@ -27,6 +27,7 @@ void	double_rotate(t_list **lst_a, t_list **lst_b, t_count *count)
 	rotate(lst_b, 'r', count);
 	write(1, "rr\n", 3);
 	count->rr += 1;
+	count->total += 1;
 }
 
 void	double_reverse_rotate(t_list **lst_a, t_list **lst_b, t_count *count)
